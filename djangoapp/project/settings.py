8a7568cbd18aab0e6e02495515bd2741db19ef15
Talles Correa <talles_correa@hotmail.com>
2023-07-18 13:41:43 -0300

    dockerfile and scripts

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
